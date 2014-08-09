7656802d53a4e468c33d8267de4f79820301f4b4
Arnold <sh8erby@knights.ucf.edu>
2014-08-09 17:21:28 -0400

    added the percentages

--- tipit/tipit/Content/app/controllers/homeController.js
+++ tipit/tipit/Content/app/controllers/homeController.js
@@ -1,27 +1,43 @@
 ﻿//angular.module('ngscaffold.controllers', [])
 angular.module('ngscaffold.controllers')
     .controller('homeController', ['$scope', function ($scope) {
-        $scope.greeting = 'Welcome Arnold';
-
-        $scope.parentScopeData = {
-            IsBordered: true,
-            people: []
-        };
-
-        $scope.here = 'hello';
-
-        $scope.parentScopeData2 = {
-            people: [{
-                FirstName: 'Brita',
-                LastName: 'McGrath',
-                Age: 1000
-            }, {
-                FirstName: 'Arnold',
-                LastName: 'Li',
-                Age: 258
+            
+        //create an array of objects to ngrepeat
+        $scope.parentScope = {
+            percent: [{
+                tenPercent: .1                
             }]
-        };
+        }
 
-     
+        $scope.$watch('total', function ()
+        {
+            var total = $scope.total;
+            //ten
+            $scope.hereTen = total * .1;
+            $scope.hereTenTotal = parseInt(total) + $scope.hereTen;
+
+            //thirteen
+            $scope.hereThirteen = total * .13;
+            $scope.hereThirteenTotal = parseInt(total) + $scope.hereThirteen;
+
+            //fifteen
+            $scope.hereFifteen = total * .15;
+            $scope.hereFifteenTotal = parseInt(total) + $scope.hereFifteen;
+
+            //eighteen
+            $scope.hereEighteen = total * .18;
+            $scope.hereEighteenTotal = parseInt(total) + $scope.hereEighteen;
+
+            //twenty
+            $scope.hereTwenty = total * .2;
+            $scope.hereTwentyTotal = parseInt(total) + $scope.hereTwenty;
+
+        })
+
+        var TotalFunction = function (percent, total) {
+            var added = parseInt(total) + percent;
+
+            return added
+        };
 
     }]);
