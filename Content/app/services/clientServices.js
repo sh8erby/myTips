5673b4aec9bb1c37ed43539aa51887cfcf865238
Arnold <sh8erby@knights.ucf.edu>
2014-08-25 15:08:04 -0400

    simpler changes

--- Content/app/services/clientServices.js
+++
@@ -1,1 +0,0 @@
-﻿angular.module('ngscaffold.services', ['ngResource'])
---
+++ homeController.js
@@ -0,0 +1,35 @@
+﻿angular.module('ngTips', [])
+  .controller('homeController', ['$scope', function ($scope) {
+      $scope.$watch('total', function () {
+          var total = $scope.total;
+          //ten
+          $scope.hereTen = total * .1;
+          $scope.hereTenTotal = parseInt(total) + $scope.hereTen;
+
+          //thirteen
+          $scope.hereThirteen = total * .13;
+          $scope.hereThirteenTotal = parseInt(total) + $scope.hereThirteen;
+
+          //fifteen
+          $scope.hereFifteen = total * .15;
+          $scope.hereFifteenTotal = parseInt(total) + $scope.hereFifteen;
+
+          //eighteen
+          $scope.hereEighteen = total * .18;
+          $scope.hereEighteenTotal = parseInt(total) + $scope.hereEighteen;
+
+          //twenty
+          $scope.hereTwenty = total * .2;
+          $scope.hereTwentyTotal = parseInt(total) + $scope.hereTwenty;
+
+      })
+
+      var TotalFunction = function (percent, total) {
+          var added = parseInt(total) + percent;
+
+          return added
+      };
+
+   
+  }]);
+
